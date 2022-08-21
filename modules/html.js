f2a1eaab3835b5a2924f2545a1c22d3fdf2aec28
Oriun <emmanuel.nuiro@epitech.eu>
2022-08-22 01:38:50 +0200

    [+] new html methods

--- modules/html.js
+++ modules/html.js
@@ -1,27 +1,33 @@
-const { prepare, hydrateStrings } = require('./clean')
-const {createHTMLElement} = require('./element')
-const XMEN_XML = require('./xml')
+const { prepare, hydrateStrings } = require("./clean");
+const { createHTMLElement } = require("./element");
+const XMEN_XML = require("./xml");
 
 function removeScripts(data) {
-    data.scripts = []
-    data.text = data.text.replace(/<script[^>]*>(?<script>(.(?!\/script>))*)<\/script>/gs, (str, sc) => {
-        if (!sc) return str
-        const index = data.scripts.push(sc) - 1
-        const start = str.slice(0, 0 - sc.length - 9)
-        return start + '_%$' + index + '</script>'
-    })
-    return data
+  data.scripts = [];
+  data.text = data.text.replace(
+    /<script[^>]*>(?<script>(.(?!\/script>))*)<\/script>/gs,
+    (str, sc) => {
+      if (!sc) return str;
+      const index = data.scripts.push(sc) - 1;
+      const start = str.slice(0, 0 - sc.length - 9);
+      return start + "_%$" + index + "</script>";
+    }
+  );
+  return data;
 }
 
 function removeStyle(data) {
-    data.styles = []
-    data.text = data.text.replace(/<style[^>]*>(?<style>(.(?!\/style>))*)<\/style>/gs, (str, st) => {
-        if (!st) return str
-        const index = data.styles.push(st) - 1
-        const start = str.slice(0, 0 - st.length - 8)
-        return start + '_&$' + index + '</style>'
-    })
-    return data
+  data.styles = [];
+  data.text = data.text.replace(
+    /<style[^>]*>(?<style>(.(?!\/style>))*)<\/style>/gs,
+    (str, st) => {
+      if (!st) return str;
+      const index = data.styles.push(st) - 1;
+      const start = str.slice(0, 0 - st.length - 8);
+      return start + "_&$" + index + "</style>";
+    }
+  );
+  return data;
 }
 
 // module.exports = {
@@ -31,52 +37,89 @@ function removeStyle(data) {
 // }
 
 const htmlOrphanTags = [
-    "meta", "br", "hr", "progress", "input", "iframe", "frame", "noframes", "img", "area", "canvas", "source", "track", "link", "base", "basefont"
-]
+  "meta",
+  "br",
+  "hr",
+  "progress",
+  "input",
+  "iframe",
+  "frame",
+  "noframes",
+  "img",
+  "area",
+  "canvas",
+  "source",
+  "track",
+  "link",
+  "base",
+  "basefont"
+];
 
 function runTree(root, extract = () => false) {
-    if (extract(root)) return;
-    if (root.$children?.length) root.$children.forEach(node => { runTree(node, extract) })
+  if (extract(root)) return;
+  if (root.$children?.length)
+    root.$children.forEach((node) => {
+      runTree(node, extract);
+    });
 }
 
 module.exports = class XMEN_HTML extends XMEN_XML {
+  styles = [];
+  scripts = [];
 
-    styles = []
-    scripts = []
+  constructor(dataInStringOrBuffer) {
+    let data;
+    if (typeof dataInStringOrBuffer === "string") {
+      data = dataInStringOrBuffer;
+    } else if (dataInStringOrBuffer instanceof Buffer) {
+      data = dataInStringOrBuffer.toString();
+    } else {
+      throw new Error("Invalid data in constructor.");
+    }
+    const { styles, text: tmp } = removeStyle({ text: data });
+    const { scripts, text } = removeScripts({ text: tmp });
+    super(text, htmlOrphanTags, createHTMLElement);
+    this.styles = styles;
+    this.scripts = scripts;
+  }
+  querySelectorLimit(selector, limit) {
+    throw new Error("Not implemented yet.");
+  }
 
-    constructor(dataInStringOrBuffer) {
-        let data;
-        if (typeof dataInStringOrBuffer === 'string') {
-            data = dataInStringOrBuffer
-        } else if (dataInStringOrBuffer instanceof Buffer) {
-            data = dataInStringOrBuffer.toString()
-        } else {
-            throw new Error("Invalid data in constructor.")
-        }
-        const { styles, text: tmp } = removeStyle({ text: data })
-        const { scripts, text } = removeScripts({ text: tmp })
-        super(text, htmlOrphanTags, createHTMLElement)
-        this.styles = styles
-        this.scripts = scripts
-    }
-    querySelectorLimit(selector, limit) {
+  querySelector(selector) {
+    return querySelectorLimit(selector, 1);
+  }
+  querySelectorAll(selector) {
+    return querySelectorLimit(selector, Infinity);
+  }
 
-    }
-
-    querySelector(selector) {
-        return querySelectorLimit(selector, 1)
-    }
-    querySelectorAll(selector) {
-        return querySelectorLimit(selector, Infinity)
-    }
-
-    getElementsByClassName(classes, max = Infinity) {
-        const classList = classes.split(' ')
-        const matches = []
-        runTree(this.root, node => {
-            if (matches.length >= max) return true
-            if (node.$attributes.class?.length && classList.every(cl => node.$attributes.class.includes(cl))) matches.push(node)
-        })
-        return matches
-    }
-}
+  getElementsByClassName(classes, max = Infinity) {
+    const classList = classes.split(" ");
+    const matches = [];
+    runTree(this.root, (node) => {
+      if (matches.length >= max) return true;
+      if (
+        node.$attributes.class?.length &&
+        classList.every((cl) => node.$attributes.class.includes(cl))
+      )
+        matches.push(node);
+    });
+    return matches;
+  }
+  getElementsByTagName(tagName, max = Infinity) {
+    const matches = [];
+    runTree(this.root, (node) => {
+      if (matches.length >= max) return true;
+      if (node.$tagName === tagName) matches.push(node);
+    });
+    return matches;
+  }
+  getElementsById(id, max = Infinity) {
+    const matches = [];
+    runTree(this.root, (node) => {
+      if (matches.length >= max) return true;
+      if (node.id === id) matches.push(node);
+    });
+    return matches[0];
+  }
+};
